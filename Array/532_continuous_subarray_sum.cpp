27be1f825ce7887c281e813a402999764a84d9ac
d7 <dhruvitpatel2811@gmail.com>
2024-06-09 19:01:23 +0530

    ...

--- Array/532_continuous_subarray_sum.cpp
+++ Array/532_continuous_subarray_sum.cpp
@@ -1,27 +1,22 @@
-// 523. Continuous Subarray Sum
-
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        //check every subarray
-        //map
-        map<int,int>mp;
         int n=nums.size();
-        int pre=0;
+        unordered_map<int,int>mp;
+        mp[0]=-1;
+        int sum=0;
         for(int i=0;i<n;i++){
-            pre+=nums[i];
-            pre%=k;
-            if(pre==0 and i>0){
-                return true;
-            }
-            if(mp.find(pre)==mp.end()){
-                mp[pre]=i;
-            }else{
-                if((i-mp[pre])>1){
-                    return true;
+            sum+=nums[i];
+            if(k!=0){
+                sum%=k;
+                if(mp.find(sum)!=mp.end()){
+                    if(i-mp[sum]>1)return true;
+                }else{
+                    mp[sum]=i;
                 }
             }
         }
+        
         return false;
     }
 };
